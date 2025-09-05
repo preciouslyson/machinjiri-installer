4d353f2c9b508e20200be88827194fbc78b7e9c8
preciouslyson <precious. lyson@gmail.com>
2025-09-05 11:31:21 +0200

    2.0

--- src/ProjectCreator.php
+++ src/ProjectCreator.php
@@ -17,12 +17,14 @@ class ProjectCreator extends Command
     {
         $this
             ->setDescription('Create a new Machinjiri application')
-            ->addArgument('name', InputArgument::REQUIRED, 'The name of the project');
+            ->addArgument('name', InputArgument::REQUIRED, 'The name of the project')
+            ->addOption('dev', null, null, 'Install the development version');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $projectName = $input->getArgument('name');
+        $isDev = $input->getOption('dev');
         $filesystem = new Filesystem();
 
         // Check if directory already exists
@@ -33,48 +35,108 @@ class ProjectCreator extends Command
 
         $output->writeln("<info>Creating Machinjiri project: $projectName</info>");
 
-        // Create project directory
-        $filesystem->mkdir($projectName);
-        chdir($projectName);
+        try {
+            // Create project directory
+            $filesystem->mkdir($projectName);
+            chdir($projectName);
 
-        // Initialize composer project
-        $process = new Process(['composer', 'init', '--name', $projectName, '--type', 'project', '--stability', 'dev', '--no-interaction']);
-        $process->run();
+            // Initialize composer project
+            $output->writeln("<info>Initializing Composer project...</info>");
+            $this->runProcess(['composer', 'init', '--name', $projectName, '--type', 'project', '--stability', 'stable', '--no-interaction'], $output);
 
-        // Require the framework
-        $process = new Process(['composer', 'require', 'mlangenigroup/machinjiri']);
-        $process->run();
+            // Require the framework
+            $output->writeln("<info>Installing Machinjiri framework...</info>");
+            $version = $isDev ? 'dev-main' : '^1.3';
+            $this->runProcess(['composer', 'require', 'mlangenigroup/machinjiri:' . $version], $output);
 
-        // Copy framework files
-        $this->setupFrameworkStructure($filesystem, $output);
+            // Set up framework structure
+            $this->setupFrameworkStructure($filesystem, $output);
 
-        $output->writeln("<info>Project $projectName created successfully!</info>");
-        $output->writeln("<comment>Run 'cd $projectName && php artisan serve' to start development server</comment>");
+            $output->writeln("<info>Project $projectName created successfully!</info>");
+            $output->writeln("<comment>Next steps:</comment>");
+            $output->writeln("<comment>  - cd $projectName</comment>");
+            $output->writeln("<comment>  - Configure your .env file</comment>");
+            $output->writeln("<comment>  - Run 'php artisan serve' to start development server</comment>");
 
-        return Command::SUCCESS;
+            return Command::SUCCESS;
+
+        } catch (\Exception $e) {
+            $output->writeln("<error>Error: " . $e->getMessage() . "</error>");
+            return Command::FAILURE;
+        }
     }
 
-    private function setupFrameworkStructure(Filesystem $filesystem, OutputInterface $output)
+    private function runProcess(array $command, OutputInterface $output): void
     {
+        $process = new Process($command);
+        $process->setTimeout(300);
+        $process->run(function ($type, $buffer) use ($output) {
+            $output->write($buffer);
+        });
+
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException("Process failed: " . $process->getErrorOutput());
+        }
+    }
+
+    private function setupFrameworkStructure(Filesystem $filesystem, OutputInterface $output): void
+    {
+        $output->writeln("<info>Setting up project structure...</info>");
+        
         $frameworkPath = 'vendor/mlangenigroup/machinjiri';
         
         // Copy environment file
         if ($filesystem->exists("$frameworkPath/.env.example")) {
             $filesystem->copy("$frameworkPath/.env.example", '.env');
+            $output->writeln("<info>Created .env file</info>");
         }
 
-        // Create directories
-        $directories = ['app/Controllers', 'app/Models', 'config', 'storage', 'public'];
+        // Create directory structure
+        $directories = [
+            'app/Controllers',
+            'app/Models',
+            'app/Views',
+            'config',
+            'storage/cache',
+            'storage/logs',
+            'storage/sessions',
+            'public/assets',
+            'resources/views',
+            'resources/assets',
+            'routes'
+        ];
+        
         foreach ($directories as $dir) {
-            $filesystem->mkdir($dir, 0755);
+            if (!$filesystem->exists($dir)) {
+                $filesystem->mkdir($dir, 0755);
+            }
         }
+        
+        // Copy default configuration if available
+        if ($filesystem->exists("$frameworkPath/config.example")) {
+            $filesystem->mirror("$frameworkPath/config.example", 'config');
+            $output->writeln("<info>Created config directory</info>");
+        }
+        
+        // Create default routes file
+        if (!$filesystem->exists('routes/web.php')) {
+            $filesystem->dumpFile('routes/web.php', "<?php\n\n// Define your routes here\n");
+            $output->writeln("<info>Created routes file</info>");
+        }
+        
+        // Generate application key
+        $this->generateAppKey($filesystem, $output);
+    }
 
-        // Generate key
-        $key = bin2hex(random_bytes(32));
-        $envContent = file_get_contents('.env');
-        $envContent = str_replace('APP_KEY=', "APP_KEY=$key", $envContent);
-        file_put_contents('.env', $envContent);
-
-        $output->writeln("<info>Framework structure created successfully!</info>");
+    private function generateAppKey(Filesystem $filesystem, OutputInterface $output): void
+    {
+        if ($filesystem->exists('.env')) {
+            $envContent = file_get_contents('.env');
+            if (strpos($envContent, 'APP_KEY=') === false) {
+                $key = base64_encode(random_bytes(32));
+                $filesystem->appendToFile('.env', "\nAPP_KEY=$key\n");
+                $output->writeln("<info>Generated application key</info>");
+            }
+        }
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,18 +10,14 @@ class ComposerStaticInitf7c3d5be38caa1afb783fa816e0dd8b5
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
-        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '8825ede83f2f289127722d4e842cf7e8' => __DIR__ . '/..' . '/symfony/polyfill-intl-grapheme/bootstrap.php',
         'e69f7f6ee287b969198c3c9d6777bd38' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/bootstrap.php',
-        '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
         'b6b991a57620e2fb6b2f66f03fe9ddc2' => __DIR__ . '/..' . '/symfony/string/Resources/functions.php',
     );
 
     public static $prefixLengthsPsr4 = array (
         'S' => 
         array (
-            'Symfony\\Polyfill\\Php80\\' => 23,
-            'Symfony\\Polyfill\\Php73\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Intl\\Normalizer\\' => 33,
             'Symfony\\Polyfill\\Intl\\Grapheme\\' => 31,
@@ -38,19 +34,11 @@ class ComposerStaticInitf7c3d5be38caa1afb783fa816e0dd8b5
         ),
         'M' => 
         array (
-            'Mlangeni\\Machinjiri\\Installer\\' => 30,
+            'Mlangeni\\Machinjiri\\Core\\Artisans\\Helpers\\' => 42,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'Symfony\\Polyfill\\Php80\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-php80',
-        ),
-        'Symfony\\Polyfill\\Php73\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-php73',
-        ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
@@ -91,21 +79,15 @@ class ComposerStaticInitf7c3d5be38caa1afb783fa816e0dd8b5
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
         ),
-        'Mlangeni\\Machinjiri\\Installer\\' => 
+        'Mlangeni\\Machinjiri\\Core\\Artisans\\Helpers\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
         ),
     );
 
     public static $classMap = array (
-        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
-        'JsonException' => __DIR__ . '/..' . '/symfony/polyfill-php73/Resources/stubs/JsonException.php',
         'Normalizer' => __DIR__ . '/..' . '/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php',
-        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
-        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
-        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
-        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
